2f9f5aac87a5a0b6a6609f42fad9a94942a9e39a
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-01-23 20:07:50 -0700

    Got basic configuration complete

---
+++ dist/script.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('Welcome to the TypeScript');
+const thor = 'Strong Avenger';
+let stark = 'Whiny science guy';
+console.log(thor, stark);
---
+++ src/script.ts
@@ -0,0 +1,9 @@
+console.log('Welcome to the TypeScript')
+
+const thor = 'Strong Avenger'
+
+let stark = 'Whiny science guy'
+
+console.log(thor, stark)
+
+
