09ad86f8342a61e097af0a2b365c9e0795cb2267
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-02-06 18:01:20 -0700

    Added Buttons

--- dist/basicTypes/basicTypes.js
+++ dist/basicTypes/basicTypes.js
@@ -1,4 +1,11 @@
 "use strict";
+const funcParamsButton = document.querySelector('#funcParams');
+const cardTitle = document.querySelector('.card-title');
+funcParamsButton === null || funcParamsButton === void 0 ? void 0 : funcParamsButton.addEventListener('click', showExample);
+function showExample() {
+    console.log('About to show example...');
+    //cardTitle?.textContent= 'function add(num1: number, num2: number)'
+}
 // a parameter is a variable passed into a function
 function add(num1, num2) {
     return num1 + num2;
@@ -8,39 +15,19 @@ const numberTwo = 3.5;
 // an argument i sa value (primitive or object) passed as an input
 let result = add(numberOne, numberTwo);
 console.log(result);
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["DEV"] = 1] = "DEV";
+    Role[Role["STUDENT"] = 2] = "STUDENT";
+})(Role || (Role = {}));
 const sportsCar = {
     make: "McLaren",
     model: "720s",
     horsepower: 720,
     style: "coupe",
+    isFast: true,
+    colors: ['red', 'blue', 'silver'],
+    driver: [1, "McLaren"],
+    role: Role.ADMIN
 };
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 5] = "ADMIN";
-    Role[Role["READ_ONLY"] = 6] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 7] = "AUTHOR";
-})(Role || (Role = {}));
-const BMP1 = {
-    //make: string
-    //model: string,
-    //horsepower: number
-    //style: string
-    //opperators: string[]
-    //  role: [number, string]
-    //} =
-    make: "BMP-1",
-    model: "1",
-    horsepower: 300,
-    style: "IFV",
-    opperators: ["Ukraine", "Iraq", "Russia"],
-    role: [2, Role.ADMIN]
-};
-//BMP1.role.push('admin')
-//BMP1.role[1] = 10
-let currentOperators;
-currentOperators = ["Ukraine"];
-console.log(BMP1.make);
-for (const country of BMP1.opperators) {
-    console.log(country.toUpperCase());
-}
-console.log("My car is", sportsCar, BMP1);
--- src/basicTypes/basicTypes.ts
+++ src/basicTypes/basicTypes.ts
@@ -1,53 +1,46 @@
-// a parameter is a variable passed into a function
-function add(num1: number, num2: number) {
-    return num1 + num2
+
+
+const funcParamsButton = document.querySelector('#funcParams')
+const cardTitle = document.querySelector('.card-title')
+
+funcParamsButton?.addEventListener('click', showExample)
+
+function showExample() {
+    console.log('About to show example...')
+    //cardTitle?.textContent= 'function add(num1: number, num2: number)'
 }
 
-const numberOne = 29
-const numberTwo = 3.5
+// a parameter is a variable passed into a function
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+const numberOne = 29;
+const numberTwo = 3.5;
 
 // an argument i sa value (primitive or object) passed as an input
 
-let result = add(numberOne, numberTwo)
-console.log(result)
+let result = add(numberOne, numberTwo);
+console.log(result);
 
-const sportsCar = {
-    make: "McLaren",
-    model: "720s",
-    horsepower: 720,
-    style: "coupe",
-}
+enum Role {ADMIN, DEV, STUDENT}
 
-enum Role { ADMIN = 5 , READ_ONLY, AUTHOR }
-
-const BMP1 = {//{
-    //make: string
-    //model: string,
-    //horsepower: number
-    //style: string
-    //opperators: string[]
-  //  role: [number, string]
-//} =
- 
-    make: "BMP-1",
-    model: "1",
-    horsepower: 300,
-    style: "IFV",
-    opperators: ["Ukraine", "Iraq", "Russia"],
-    role: [2, Role.ADMIN]
-}
-
-//BMP1.role.push('admin')
-//BMP1.role[1] = 10
-
-let currentOperators: string[]
-currentOperators = ["Ukraine"]
-
-console.log(BMP1.make)
-
-for (const country of BMP1.opperators) {
-    console.log(country.toUpperCase())
-}
-
-console.log("My car is", sportsCar, BMP1)
-
+const sportsCar: { 
+    make: string; 
+    model: string;
+    horsepower: number;
+    style: string;
+    isFast: boolean;
+    colors: string[];
+    driver: [1, string];
+    role: Role;
+} = {
+  make: "McLaren",
+  model: "720s",
+  horsepower: 720,
+  style: "coupe",
+  isFast: true,
+  colors: ['red', 'blue', 'silver'],
+  driver: [1, "McLaren"],
+  role: Role.ADMIN
+};
