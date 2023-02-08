a79f78b2f272185cf1f912e23d5495b46f958a14
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-02-07 20:50:55 -0700

    Use non-null assertion

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 //import { setupCounter } from "./counter"
 
-document.querySelector('#counterDiv')!.innerHTML = `
-<a class="text-3xl" href="https://www.uvu.edu" target="_blank">Go to UVU!</a>`
+//document.querySelector('#counterDiv')!.innerHTML = `
+//<a class="text-3xl" href="https://www.uvu.edu" target="_blank">Go to UVU!</a>`
 
+export function logSomething() {
+    console.log('Main index.ts reporting for duty!')
+}
