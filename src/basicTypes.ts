35b2c3f8422da5a63840d365742c231f85fe454d
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-02-21 13:18:19 -0700

    Added Object Types Example Btn

--- src/basicTypes.ts
+++ src/basicTypes.ts
@@ -6,16 +6,36 @@ export const functionParamsExample = () => {
       code: `<pre data-prefix="1"><code>function add(num1; number, num2: number) {</code></pre>
 
         <pre data-prefix="2"><code>  return num1 + num2</code></pre> 
-        <pre data-prefix="3"><code>}</code></pre>`
+        <pre data-prefix="3"><code></code></pre>`
     }
 
 }
 
 export const objectTypesExample = () => {
   return {
-    title: ``,
-    explanation: ``,
-    code:``
+    title: `Object data type with all properties set`,
+    explanation: `TypeScript can infer the data type of an object if all properties are set, but this example shows how to explicitly set the types of each property.`,
+    code:`<pre data-prefix="1"><code> enum Role {ADMIN, DEV, STUDENT}</code></pre>
+          <pre data-prefix="2"><code></code></pre>
+          <pre data-prefix="3"><code>const sportsCar: {</code></pre>
+          <pre data-prefix="4"><code> make: string</code></pre>
+          <pre data-prefix="5"><code> model: string</code></pre>
+          <pre data-prefix="6"><code> horsepower: number</code></pre>
+          <pre data-prefix="7"><code> stype: string</code></pre>
+          <pre data-prefix="8"><code> isFast: boolean</code></pre>
+          <pre data-prefix="9"><code> colors: string</code></pre>
+          <pre data-prefix="10"><code> driver: [number, string]</code></pre>
+          <pre data-prefix="11"><code> role: Role</code></pre>
+          <pre data-prefix="12"><code> } = </code></pre>
+          <pre data-prefix="13"><code> make: 'McLaren',</code></pre>
+          <pre data-prefix="14"><code> model: '720s',</code></pre>
+          <pre data-prefix="15"><code> horsepower: 720,</code></pre>
+          <pre data-prefix="16"><code> style: 'coupe',</code></pre>
+          <pre data-prefix="17"><code> isFast: true,</code></pre>
+          <pre data-prefix="18"><code> colors: ['red', 'blue', 'silver']</code></pre>
+          <pre data-prefix="19"><code> driver: [1, 'McLaren']</code></pre>
+          <pre data-prefix="20"><code> role: Role.ADMIN,</code></pre>
+          <pre data-prefix="21"><code> }</code></pre> `
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { functionParamsExample } from "./basicTypes"
+import { objectTypesExample } from "./basicTypes"
 
 const funcParamsButton = document.querySelector('#funcParams')!
 const objectTypesButton = document.querySelector('#objectTypes')!
@@ -8,11 +9,18 @@ const cardContent = document.querySelector('#card-content')!
 const codeContent = document.querySelector('#code-content')!
 
 funcParamsButton.addEventListener('click', showFuncParamsExample)
-//objectTypesButton.addEventListener('click', showExample2)
+objectTypesButton.addEventListener('click', showObjectTypesExample)
 
 function showFuncParamsExample() {
     cardTitle.textContent = functionParamsExample().title // chaining example
     const { explanation, code } = functionParamsExample()
+    cardContent.textContent = explanation
+    codeContent.innerHTML = code
+}
+
+function showObjectTypesExample() {
+    cardTitle.textContent = objectTypesExample().title // chaining example
+    const { explanation, code } = objectTypesExample()
     cardContent.textContent = explanation
     codeContent.innerHTML = code
 }
