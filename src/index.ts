91ac9499ae0fda7fb73a3d4da826d04adde592ff
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-02-21 12:53:36 -0700

    Text content and inner HTML for funcParamsExample

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { functionParamsExample } from "./basicTypes"
+
 const funcParamsButton = document.querySelector('#funcParams')!
 const objectTypesButton = document.querySelector('#objectTypes')!
 const codeExample = document.querySelector('#code-example')!
@@ -5,5 +7,14 @@ const cardTitle = document.querySelector('.card-title')!
 const cardContent = document.querySelector('#card-content')!
 const codeContent = document.querySelector('#code-content')!
 
-//funcParamsButton?.addEventListener('click', showExample1)
+funcParamsButton.addEventListener('click', showFuncParamsExample)
 //objectTypesButton.addEventListener('click', showExample2)
+
+function showFuncParamsExample() {
+    //console.log('About to show function params data...')
+    //console.log(functionParamsExample())
+    cardTitle.textContent = functionParamsExample().title // chaining example
+    const { explaination, code } = functionParamsExample()
+    cardContent.textContent = explaination
+    codeContent.innerHTML = code
+}
