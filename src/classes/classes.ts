7c3edf800d6c7df141aecb7df7b1a857bdc71882
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-03-01 17:40:35 -0700

    SnippetDisplay

--- src/classes/classes.ts
+++ src/classes/classes.ts
@@ -1,62 +1,72 @@
-import exampleData from '../data/exampleData.json'
+import curriculum from "../data/curriculum.json";
 
-const buildUIFromJSON = (dataObj: any) => {
-    dataObj.data.forEach((item: any) => {
-        console.log(item)
+const blocks = curriculum["responsive-web-design"].blocks;
+
+//console.log('Blocks', blocks)
+
+const blocksArray = Object.values(blocks);
+
+//console.log('Blocks Array', blocksArray)
+
+//console.log(blocksArray[0].meta.name)
+
+const navBar = document.querySelector(".navbar")!;
+const navList = document.querySelector("#navList")!;
+
+
+const buildNavFromJSON = (blockName: string) => {
+  const navItem = document.createElement("a");
+  navItem.classList.add("btn", "btn-ghost", "normal-case", "text-xl", "mx-4");
+  navItem.textContent = blockName;
+  navItem.addEventListener("click", () => {
+    const foundBlock = blocksArray.find(
+      (item: any) => item.meta.name === blockName
+    );
+    console.log(foundBlock);
+    buildLeftNavFromJSON(foundBlock);
+  });
+  navBar.appendChild(navItem);
+};
+
+blocksArray.forEach((block) => {
+  buildNavFromJSON(block.meta.name);
+});
+
+const buildLeftNavFromJSON = (block: any) => {
+    while (navList.firstChild) {
+        navList.removeChild(navList.firstChild)
+    }
+  block.challenges.forEach((challenge: any) => {
+    //console.log(chanllenge)
+    const listItem = document.createElement("li");
+    listItem.classList.add("my-4");
+    const navButton = document.createElement("h3");
+    navButton.classList.add("w-72", "bg-blue-200", "rounded", "p-2", "text-xl", "text-center", "cursor-pointer", "hover:bg-blue-300", "hover:text-white", "shadow-medium");
+    navButton.textContent = challenge.title;
+    listItem.addEventListener('click', () => {
+        buildSnippetDisplay(challenge)
     })
-}
-
-buildUIFromJSON(exampleData)
+    listItem.appendChild(navButton);
+    navList.appendChild(listItem);
+  });
+};
 
 class ExampleCodeSnippet {
-    private title: string
-    private explanation: string
-    private code: string
+    constructor(public title: string, public description: string, public instructions: string) {
 
-    constructor(title: string, explanation: string, code: string) {
-        this.title = title
-        this.explanation = explanation
-        this.code = code
-    }
-
-    getTitle() {
-        return this.title
-    }
-
-    getExplanation() {
-        return this.explanation
-    }
-
-    getCode() {
-        return this.code
     }
 }
 
-class ExampleCodeSnippetShortcut {
-    constructor(private title: string, private explanation: string, private code: string ) {
+const buildSnippetDisplay = (challenge: any) => {
+    const snippet = new ExampleCodeSnippet(challenge.title, challenge.description, challenge.instructions)
+    console.log(snippet)
+    const snippetDisplay = document.querySelector('#snippetDisplay')!
+    while (snippetDisplay.firstChild) {
+        snippetDisplay.removeChild(snippetDisplay.firstChild)
     }
+    const snippetTitle = document.createElement('h2')
+    snippetTitle.classList.add('text-2xl', 'font-bold', 'mb-4')
+    snippetTitle.textContent = snippet.title
 
-    getTitle() {
-        return this.title
-    }
-
-    getExplanation() {
-        return this.explanation
-    }
-
-    getCode() {
-        return this.code
-    }
+    snippetDisplay.appendChild(snippetTitle)
 }
-
-let codeSnippet1 = new ExampleCodeSnippet(`How to define a class`, `Here is where I explain how to define class`, `<pre data-prefix="1"><code><h1>Hello</h1></code></pre>`)
-
-
-console.log(codeSnippet1.getTitle())
-console.log(codeSnippet1.getExplanation())
-
-console.log(Math.random())
-
-let simplerCodeSnipppet = new ExampleCodeSnippetShortcut(`How to define a simpler shortcut`, `Here is where I explain how to define a more simple shortcut using the constructor`, `<pre data-prefix="1"><code> Class ExampleCodeSnippet</code></pre>`)
-
-console.log(simplerCodeSnipppet.getTitle)
