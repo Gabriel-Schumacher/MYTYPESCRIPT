dbb7be0f9fe8009da07169dee09c2b4567d62d19
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-03-27 17:24:19 -0600

    Added video frame

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -12,18 +12,32 @@ interface VideoDisplayer {
 }
 
 class CodeSnippet implements VideoDisplayer {
-    constructor(public title: string, public description: string, public instruction: string, public videoURL: string) {
+    constructor(public title: string, public description: string, public instruction: string, public videoUrl: string) {
     }
     display(theFrame: HTMLIFrameElement): void {
-        theFrame.src = this.videoURL
+        theFrame.src = this.videoUrl
     }
 }
 
 const buildSnippetDisplay = (challenge: any) => {
-    const snippet = new CodeSnippet(challenge.title, challenge.description, challenge.instruction, challenge.videoURL)
+    const snippet = new CodeSnippet(challenge.title, challenge.description, challenge.instruction, challenge.videoUrl)
 
     const displayCard = document.createElement('div')
-    displayCard.classList.add('bg-white', 'rounded', 'shadow-md', 'p-4', 'm-4', 'w-1/2')
+    displayCard.classList.add('bg-white', 'rounded', 'shadow-md', 'p-4', 'm-4', 'w-1/2', 'text-center')
 
     const videoFrame = document.createElement('iframe')
+    videoFrame.classList.add('w-[1024px]', 'h-[600px]', 'rounded', 'shadow-md', 'm-1')
+
+    const title = document.createElement('h2')
+    title.classList.add('text-2xl', 'font-bold', 'm-2', 'btn')
+    title.textContent = snippet.title
+    title.addEventListener('click', () => {
+        snippet.display(videoFrame)
+    })
+
+    displayCard.appendChild(videoFrame)
+    displayCard.appendChild(title)
+    main.appendChild(displayCard)
 }
+
+buildSnippetDisplay(blocksArray[0].challenges[0])
