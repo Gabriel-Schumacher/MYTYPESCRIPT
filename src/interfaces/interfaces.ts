f9930fce0b9e3a0f5d41ae433b01003867aaaa64
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-03-28 01:36:21 -0600

    Messing with UI

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -3,8 +3,13 @@ import curriculum from '../data/curriculum.json'
 const blocks = curriculum['responsive-web-design'].blocks
 
 const blocksArray = Object.values(blocks)
+const [basicCss, basicHTML, flexbox, grid] = blocksArray
 
-const main = document.querySelector('main')!
+console.log(basicCss)
+
+const displayFrame = document.querySelector('.displayFrame')!
+const prevButton = document.querySelector('#prevButton')
+const nextButton = document.querySelector('#nextButton')
 
 interface VideoDisplayer {
     videoUrl: string
@@ -19,16 +24,20 @@ class CodeSnippet implements VideoDisplayer {
     }
 }
 
-const buildSnippetDisplay = (challenge: any) => {
+const buildSnippetDisplay = (challenges: object[]) => {
+    
+}
+
+/* const buildSnippetDisplay = (challenge: any) => {
     const snippet = new CodeSnippet(challenge.title, challenge.description, challenge.instruction, challenge.videoUrl)
 
-    const displayCard = document.createElement('div')
-    displayCard.classList.add('bg-white', 'rounded', 'shadow-md', 'p-4', 'm-4', 'w-1/2', 'text-center')
+    /* const displayCard = document.createElement('div')
+    displayCard.classList.add('bg-white', 'rounded', 'shadow-md', 'p-4', 'm-4', 'w-1/2', 'text-center') */
 
-    const videoFrame = document.createElement('iframe')
-    videoFrame.classList.add('w-[1024px]', 'h-[600px]', 'rounded', 'shadow-md', 'm-1')
+    /* const videoFrame = document.createElement('iframe')
+    videoFrame.classList.add('w-[1024px]', 'h-[600px]', 'rounded', 'shadow-md', 'm-1') */
 
-    const title = document.createElement('h2')
+    /* const title = document.createElement('h2')
     title.classList.add('text-2xl', 'font-bold', 'm-2', 'btn')
     title.textContent = snippet.title
     title.addEventListener('click', () => {
@@ -37,7 +46,7 @@ const buildSnippetDisplay = (challenge: any) => {
 
     displayCard.appendChild(videoFrame)
     displayCard.appendChild(title)
-    main.appendChild(displayCard)
-}
+    displayFrame.appendChild(displayCard) 
+} */
 
 buildSnippetDisplay(blocksArray[0].challenges[0])
