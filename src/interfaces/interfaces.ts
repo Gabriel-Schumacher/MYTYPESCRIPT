32347207b81d697b8785a330c897ed202016e4ac
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-04-06 14:19:10 -0600

    Added objects from blocksarray and consol logged

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -5,11 +5,11 @@ const blocks = curriculum['responsive-web-design'].blocks
 const blocksArray = Object.values(blocks)
 const [basicCss, basicHTML, flexbox, grid] = blocksArray
 
-console.log(basicCss)
+console.log(basicCss, basicHTML, flexbox, grid)
 
 const displayFrame = document.querySelector('.displayFrame')!
-const prevButton = document.querySelector('#prevButton')
-const nextButton = document.querySelector('#nextButton')
+const prevButton = document.querySelector('#prevButton')!
+const nextButton = document.querySelector('#nextButton')!
 
 interface VideoDisplayer {
     videoUrl: string
@@ -29,24 +29,24 @@ const buildSnippetDisplay = (challenges: object[]) => {
 }
 
 /* const buildSnippetDisplay = (challenge: any) => {
-    const snippet = new CodeSnippet(challenge.title, challenge.description, challenge.instruction, challenge.videoUrl)
+   const snippet = new CodeSnippet(challenge.title, challenge.description, challenge.instruction, challenge.videoUrl)
 
-    /* const displayCard = document.createElement('div')
-    displayCard.classList.add('bg-white', 'rounded', 'shadow-md', 'p-4', 'm-4', 'w-1/2', 'text-center') */
+   const displayCard = document.createElement('div')
+   displayCard.classList.add('bg-white', 'rounded', 'shadow-md', 'p-4', 'm-4', 'w-1/2', 'text-center') 
 
-    /* const videoFrame = document.createElement('iframe')
-    videoFrame.classList.add('w-[1024px]', 'h-[600px]', 'rounded', 'shadow-md', 'm-1') */
+   const videoFrame = document.createElement('iframe')
+   videoFrame.classList.add('w-[1024px]', 'h-[600px]', 'rounded', 'shadow-md', 'm-1') 
 
-    /* const title = document.createElement('h2')
-    title.classList.add('text-2xl', 'font-bold', 'm-2', 'btn')
-    title.textContent = snippet.title
-    title.addEventListener('click', () => {
-        snippet.display(videoFrame)
-    })
+   const title = document.createElement('h2')
+   title.classList.add('text-2xl', 'font-bold', 'm-2', 'btn')
+   title.textContent = snippet.title
+   title.addEventListener('click', () => {
+       snippet.display(videoFrame)
+   })
 
     displayCard.appendChild(videoFrame)
     displayCard.appendChild(title)
     displayFrame.appendChild(displayCard) 
 } */
 
-buildSnippetDisplay(blocksArray[0].challenges[0])
+//buildSnippetDisplay(blocksArray[0].challenges[0]) 
