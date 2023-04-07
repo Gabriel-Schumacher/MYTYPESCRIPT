fb487a077e5a9ebd296fcdf06845a3099f56e698
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-04-07 01:03:35 -0600

    Did some more styling

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,158 +1,180 @@
-import curriculum from '../data/curriculum.json'
+import curriculum from "../data/curriculum.json";
 
-const blocks = curriculum['responsive-web-design'].blocks
+const blocks = curriculum["responsive-web-design"].blocks;
 
-const blocksArray = Object.values(blocks)
-const [basicCss, basicHTML, flexbox, grid] = blocksArray
+const blocksArray = Object.values(blocks);
+const [basicCss, basicHTML, flexbox, grid] = blocksArray;
 
-let cssPosition: number = 0
-let htmlPosition: number = 0
-let flexboxPosition: number = 0
-let gridPosition: number = 0
+let cssPosition: number = 0;
+let htmlPosition: number = 0;
+let flexboxPosition: number = 0;
+let gridPosition: number = 0;
 
-let currentBlock: string
+let currentBlock: string;
 
-const videoFrame = document.querySelector('#challengeVid')! as HTMLIFrameElement 
-const prevButton = document.querySelector('#prevButton')! as HTMLButtonElement
-const nextButton = document.querySelector('#nextButton')! as HTMLButtonElement
-const challengeVidBtn = document.querySelector('#challengeVidBtn')!
-const topMenu = document.querySelector('.menu')!
+const videoFrame = document.querySelector(
+  "#challengeVid"
+)! as HTMLIFrameElement;
+const prevButton = document.querySelector("#prevButton")! as HTMLButtonElement;
+const nextButton = document.querySelector("#nextButton")! as HTMLButtonElement;
+const challengeVidBtn = document.querySelector("#challengeVidBtn")!;
+const topMenu = document.querySelector(".menu")!;
 
 interface VideoDisplayer {
-    videoUrl: string
-    display(theFrame: HTMLIFrameElement): void
+  videoUrl: string;
+  display(theFrame: HTMLIFrameElement): void;
 }
 
 class CodeSnippet implements VideoDisplayer {
-    constructor(public title: string, public description: string, public instruction: string, public videoUrl: string) {
-    }
-    display(theFrame: HTMLIFrameElement): void {
-        theFrame.src = this.videoUrl
-    }
+  constructor(
+    public title: string,
+    public description: string,
+    public instruction: string,
+    public videoUrl: string
+  ) {}
+  display(theFrame: HTMLIFrameElement): void {
+    theFrame.src = this.videoUrl;
+  }
 }
 
 const basicCssSnippets = basicCss.challenges.map((challenge: any) => {
-    return new CodeSnippet(challenge.title, challenge.description, challenge.instructions, challenge.videoUrl)
-})
+  return new CodeSnippet(
+    challenge.title,
+    challenge.description,
+    challenge.instructions,
+    challenge.videoUrl
+  );
+});
 
 const basicHTMLSnippets = basicHTML.challenges.map((challenge: any) => {
-    return new CodeSnippet(challenge.title, challenge.description, challenge.instructions, challenge.videoUrl)
-})
+  return new CodeSnippet(
+    challenge.title,
+    challenge.description,
+    challenge.instructions,
+    challenge.videoUrl
+  );
+});
 
 const flexboxSnippets = flexbox.challenges.map((challenge: any) => {
-    return new CodeSnippet(challenge.title, challenge.description, challenge.instructions, challenge.videoUrl)
-})
+  return new CodeSnippet(
+    challenge.title,
+    challenge.description,
+    challenge.instructions,
+    challenge.videoUrl
+  );
+});
 
 const gridSnippets = grid.challenges.map((challenge: any) => {
-    return new CodeSnippet(challenge.title, challenge.description, challenge.instructions, challenge.videoUrl)
-})
+  return new CodeSnippet(
+    challenge.title,
+    challenge.description,
+    challenge.instructions,
+    challenge.videoUrl
+  );
+});
 
 const populateMenu = (arrayOfBlocks: any[]) => {
-    arrayOfBlocks.forEach(block => {
-        const menuItem = document.createElement('li')
-        const menuLink = document.createElement('a')
-        menuLink.textContent = block?.meta?.name
+  arrayOfBlocks.forEach((block) => {
+    const menuItem = document.createElement("li");
+    const menuLink = document.createElement("a");
+    menuLink.textContent = block?.meta?.name;
 
-        menuLink.addEventListener('click', () => {
-            const allMenuLinks = document.querySelectorAll('.menu li a')
-            allMenuLinks.forEach(link => {
-                link.classList.remove('active')
-            })
-            menuLink.classList.add('active')
-            currentBlock = block?.meta?.name
-            const snippet = getCurrentSnippet(currentBlock)
-            if (snippet) {
-                snippet.display(videoFrame)
-                challengeVidBtn.textContent = snippet.title
-            }
-        })
+    menuLink.addEventListener("click", () => {
+      const allMenuLinks = document.querySelectorAll(".menu li a");
+      allMenuLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+      menuLink.classList.add("active");
+      currentBlock = block?.meta?.name;
+      const snippet = getCurrentSnippet(currentBlock);
+      if (snippet) {
+        snippet.display(videoFrame);
+        challengeVidBtn.textContent = snippet.title;
+      }
+    });
 
-        menuItem.appendChild(menuLink)
-        topMenu.appendChild(menuItem)
-    })
-}
+    menuItem.appendChild(menuLink);
+    topMenu.appendChild(menuItem);
+  });
+};
 
-populateMenu(blocksArray)
+populateMenu(blocksArray);
 
 const getCurrentSnippet = (block: string) => {
-    if (block === 'Basic CSS') {
-        return basicCssSnippets[cssPosition]
-    }
-    else if (block === 'Basic HTML and HTML5') {
-        return basicHTMLSnippets[htmlPosition]
-    }
-    else if (block === 'CSS Flexbox') {
-        return flexboxSnippets[flexboxPosition]
-    }
-    else if (block === 'CSS Grid') {
-        return gridSnippets[gridPosition]
-    } else {
-        return new CodeSnippet('','','','')
-    }
-}
+  if (block === "Basic CSS") {
+    return basicCssSnippets[cssPosition];
+  } else if (block === "Basic HTML and HTML5") {
+    return basicHTMLSnippets[htmlPosition];
+  } else if (block === "CSS Flexbox") {
+    return flexboxSnippets[flexboxPosition];
+  } else if (block === "CSS Grid") {
+    return gridSnippets[gridPosition];
+  } else {
+    return new CodeSnippet("", "", "", "");
+  }
+};
 
 const updatePosition = (direction: string) => {
-    if (currentBlock === 'Basic CSS') {
-      if (direction === 'next') {
-        if (cssPosition < basicCssSnippets.length - 1) {
-          cssPosition++
-        }
-      } else {
-        if (cssPosition > 0) {
-          cssPosition--
-        }
+  if (currentBlock === "Basic CSS") {
+    if (direction === "next") {
+      if (cssPosition < basicCssSnippets.length - 1) {
+        cssPosition++;
       }
-    }
-    if (currentBlock === 'Basic HTML and HTML5') {
-      if (direction === 'next') {
-        if (htmlPosition < basicHTMLSnippets.length - 1) {
-          htmlPosition++
-        }
-      } else {
-        if (htmlPosition > 0) {
-          htmlPosition--
-        }
-      }
-    }
-    if (currentBlock === 'CSS Flexbox') {
-      if (direction === 'next') {
-        if (flexboxPosition < flexboxSnippets.length - 1) {
-          flexboxPosition++
-        }
-      } else {
-        if (flexboxPosition > 0) {
-          flexboxPosition--
-        }
-      }
-    }
-    if (currentBlock === 'CSS Grid') {
-      if (direction === 'next') {
-        if (gridPosition < gridSnippets.length - 1) {
-          gridPosition++
-        }
-      } else {
-        if (gridPosition > 0) {
-          gridPosition--
-        }
+    } else {
+      if (cssPosition > 0) {
+        cssPosition--;
       }
     }
   }
-
-
-nextButton.addEventListener('click', () => {
-    updatePosition('next')
-    const snippet = getCurrentSnippet(currentBlock)
-    if(snippet) {
-        snippet.display(videoFrame)
-        challengeVidBtn.textContent = snippet.title
+  if (currentBlock === "Basic HTML and HTML5") {
+    if (direction === "next") {
+      if (htmlPosition < basicHTMLSnippets.length - 1) {
+        htmlPosition++;
+      }
+    } else {
+      if (htmlPosition > 0) {
+        htmlPosition--;
+      }
     }
-})
-
-prevButton.addEventListener('click', () => {
-    updatePosition('prev')
-    const snippet = getCurrentSnippet(currentBlock)
-    if(snippet) {
-        snippet.display(videoFrame)
-        challengeVidBtn.textContent = snippet.title
+  }
+  if (currentBlock === "CSS Flexbox") {
+    if (direction === "next") {
+      if (flexboxPosition < flexboxSnippets.length - 1) {
+        flexboxPosition++;
+      }
+    } else {
+      if (flexboxPosition > 0) {
+        flexboxPosition--;
+      }
     }
-})
+  }
+  if (currentBlock === "CSS Grid") {
+    if (direction === "next") {
+      if (gridPosition < gridSnippets.length - 1) {
+        gridPosition++;
+      }
+    } else {
+      if (gridPosition > 0) {
+        gridPosition--;
+      }
+    }
+  }
+};
+
+nextButton.addEventListener("click", () => {
+  updatePosition("next");
+  const snippet = getCurrentSnippet(currentBlock);
+  if (snippet) {
+    snippet.display(videoFrame);
+    challengeVidBtn.textContent = snippet.title;
+  }
+});
+
+prevButton.addEventListener("click", () => {
+  updatePosition("prev");
+  const snippet = getCurrentSnippet(currentBlock);
+  if (snippet) {
+    snippet.display(videoFrame);
+    challengeVidBtn.textContent = snippet.title;
+  }
+});
