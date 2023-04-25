4df9579c2b381fc78e5b21ac7d0d36debab84305
Gabriel-Schumacher <schumachergabe@gmail.com>
2023-04-24 20:19:48 -0600

    Added data storage

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -11,4 +11,28 @@ function reverse<T>(array: T[]): T[] {
     return array.reverse()
 }
 
-const stringArray = 
+const stringArray = ['Key Lime', 'cherry', 'lemon']
+const reversedStringArray = reverse<string>(stringArray)
+console.log(reversedStringArray)
+
+const numberArray = [1342, 2664, 34236]
+const reversedNumberArray = reverse<number>(numberArray)
+console.log(reversedNumberArray)
+
+class DataStorage<T> {
+    data: T
+
+    constructor(data: T) {
+        this.data = data
+    }
+
+    getData(): T {
+        return this.data
+    }
+}
+
+const textStorage = new DataStorage<string>('hello world')
+console.log(textStorage.getData())
+
+const numberStorage = new DataStorage<number>(1354325345)
+console.log(numberStorage.getData())
